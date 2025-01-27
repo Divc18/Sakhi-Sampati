481027c9b5e3111b4434520b96b0fb5a1458bfa7
Divya Chauhan <magicaworld18@gmail.com>
2025-01-27 21:30:23 +0530

    photo upload page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DomainSelection from "./components/DomainSelection";
 import MentorLogin from "./components/MentorLogin";
+import PhotoUpload from "./components/PhotoUpload";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<DomainSelection />} />
         <Route path="/mentor-login" element={<MentorLogin />} />
+        <Route path="/photo-upload" element={<PhotoUpload />} />
       </Routes>
     </Router>
   );
