91a4ac9c9700ad25b51f4aff40004366d38f6fc5
Divc18 <magicaworld18@gmail.com>
2025-01-27 22:12:05 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import DomainSelection from "./components/DomainSelection";
 import MentorLogin from "./components/MentorLogin";
+import PhotoUpload from "./components/PhotoUpload";
+import UserLogin from './components/UserLogin';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/" element={<DomainSelection />} />
         <Route path="/mentor-login" element={<MentorLogin />} />
+        <Route path="/photo-upload" element={<PhotoUpload />} />
+        <Route path="/user-login" element={<UserLogin />} />
       </Routes>
     </Router>
   );
